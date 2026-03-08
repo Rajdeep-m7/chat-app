5ce50c658d68b35be3bfcae028c146a8d465f574
rajdeep Majumdar <amazumder781@gmail.com>
2026-03-08 23:32:57 +0530

    add  build script

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin: ["http://localhost:5173", "https://TalkSy.onrender.com"],
   credentials: true,
-}
-))
+}));
 
 const PORT = process.env.PORT;
 
